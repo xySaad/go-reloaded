226cbd1de53e9f06b8f552cdb9a7ab0aa748ec41
xySaad <saadrm.dev@gmail.com>
2024-09-07 18:54:39 +0100

    Fix: opening quotes on words like don't/ain't

--- utils/format.go
+++ utils/format.go
@@ -8,11 +8,11 @@ import (
 func FormatTxt(txt string) []string {
 	result := txt
 
-	commaRe := regexp.MustCompile(`[\s]*(,|\...|,|!|\?|:)[.]*[\s]*`)
+	commaRe := regexp.MustCompile(`[\s]*(,|\.\.\.|,|!|\?|:)[.]*[\s]*`)
 	result = commaRe.ReplaceAllString(result, `$1 `)
 
-	quotationRe := regexp.MustCompile(`'[\s]*(.*?)[\s]*'`)
-	result = quotationRe.ReplaceAllString(result, `'$1'`)
+	quotationRe := regexp.MustCompile(`([^n])'[\s]*(.*?)[\s]*'`)
+	result = quotationRe.ReplaceAllString(result, `$1'$2'`)
 
 	re := regexp.MustCompile(` \((hex|bin|up|low|cap), (\d*)?\)(,)?`)
 	result = re.ReplaceAllString(result, `$3 ($1-$2)`)
