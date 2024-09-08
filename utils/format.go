c6e13678ea367f06667f48d26822387ab204c8aa
xySaad <saadrm.dev@gmail.com>
2024-09-08 23:26:39 +0100

    Fix: quotation and a to an

--- utils/format.go
+++ utils/format.go
@@ -11,11 +11,15 @@ func FormatTxt(txt string) []string {
 	commaRe := regexp.MustCompile(`[\s]*([,|\.\.\.|,|!|\?|:]+)[.]*[\s]*([,|\.\.\.|,|!|\?|:]*)`)
 	result = commaRe.ReplaceAllString(result, `$1$2 `)
 
-	quotationRe := regexp.MustCompile(`([^n])'[\s]*(.*?)[\s]*'`)
-	result = quotationRe.ReplaceAllString(result, `$1'$2'`)
+	apstropheRe := regexp.MustCompile(`(\w)'(\w)`)
+	result = apstropheRe.ReplaceAllString(result, `$1’$2`)
 
-	a2anRe := regexp.MustCompile(`(a|A)([\s]+[a|e|i|o|u|h|A|E|I|O|U|H])`)
+	quotationRe := regexp.MustCompile(`'[\s]*(.*?)[\s]*'`)
+	result = quotationRe.ReplaceAllString(result, `'$1'`)
+
+	a2anRe := regexp.MustCompile(`( a|A)([\s]+[a|e|i|o|u|h|A|E|I|O|U|H])`)
 	result = a2anRe.ReplaceAllString(result, `${1}n $2`)
+
 	re := regexp.MustCompile(` \((hex|bin|up|low|cap), (\d*)?\)([,|\.\.\.|,|!|\?|:]*)?`)
 	result = re.ReplaceAllString(result, `$3 ($1-$2)`)
 
