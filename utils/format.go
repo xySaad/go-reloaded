3047d1e91e0bbff4f2063befb952caef325a820e
xySaad <saadrm.dev@gmail.com>
2024-09-12 21:43:48 +0100

    collapse all punctuations

--- utils/format.go
+++ utils/format.go
@@ -8,8 +8,8 @@ import (
 func FormatTxt(txt string) []string {
 	result := txt
 
-	commaRe := regexp.MustCompile(`[ ]*([,|\.\.\.|,|!|\?|:]+)[.]*[ ]*([,|\.\.\.|,|!|\?|:]*)`)
-	result = commaRe.ReplaceAllString(result, `$1$2 `)
+	commaRe := regexp.MustCompile(`[ ]*([,\.!\?:]+)[ ]*`)
+	result = commaRe.ReplaceAllString(result, `$1`)
 
 	apstropheRe := regexp.MustCompile(`(\w)'(\w)`)
 	result = apstropheRe.ReplaceAllString(result, `$1’$2`)
@@ -17,7 +17,7 @@ func FormatTxt(txt string) []string {
 	quotationRe := regexp.MustCompile(`'[ ]*(.*?)[ ]*'`)
 	result = quotationRe.ReplaceAllString(result, `'$1'`)
 
-	a2anRe := regexp.MustCompile(`( a|A)([ ]+[a|e|i|o|u|h|A|E|I|O|U|H])`)
+	a2anRe := regexp.MustCompile(`(^a|^A| a| A)([ ]+[a|e|i|o|u|h|A|E|I|O|U|H])`)
 	result = a2anRe.ReplaceAllString(result, `${1}n $2`)
 
 	re := regexp.MustCompile(` \((hex|bin|up|low|cap)(?:, (\d+)?)?\)([,|\.\.\.|,|!|\?|:]*)?`)
